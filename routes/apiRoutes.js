3e25679f1dd00de94a9fc5e188a895c899f3b8c6
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-10-21 02:31:41 -0400

    added two new methods/routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,24 +2,49 @@ const fs = require("fs");
 const path = require("path");
 // const json = require("json")
 
-const notesDBFile = path.join(__dirname, "../db/db.json");
+const dbFile = path.join(__dirname, "../db/db.json");
 
+const readDBFile = (dbFile) => {
+    try {
+        const data = fs.readFileSync(dbFile, "utf8");
+        return JSON.parse(data);
+    } catch (e) {
+        throw e;
+    }
+}
+
+const updateDBFile = (dbFile, data) => {
+    try {
+        const result = fs.writeFileSync(dbFile, data);
+        return result;
+    } catch (e) {
+        throw e;
+    }
+
+}
 
 module.exports = function (app) {
     app.get("/api/notes", (req, res) => {
-        let notesJSON = {};
-        fs.readFile(notesDBFile, "utf8", (err, data) => {
-            if (err) {
-                throw err;
-            }
-            notesJSON = JSON.parse(data);
-            console.log(notesJSON);
-            res.json(notesJSON);
-        });
+        const notesJSON = readDBFile(dbFile);
+        console.log(notesJSON);
+        res.json(notesJSON);
     });
 
     app.post("/api/notes", (req, res) => {
         const newNote = req.body;
+        const notesJSON = readDBFile(dbFile);
+        notesJSON.push(newNote);
+        updateDBFile(dbFile, JSON.stringify(notesJSON));
+        res.json(newNote);
+    });
+
+    app.delete("/api/notes/:id", (req, res) => {
+        const delNote = req.body;
+        const delNoteId = req.params.id;
+        const notesJSON = readDBFile(dbFile);
+        notesJSON.splice(delNoteId, 1);
+        updateDBFile(dbFile, JSON.stringify(notesJSON));
+        res.json(notesJSON);
     })
 
 
