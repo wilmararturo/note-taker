2da69d7380c77fcc20cf88337fe323acb016a831
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-10-21 02:52:36 -0400

    added setings for Heroku deployment

--- server.js
+++ server.js
@@ -9,16 +9,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(express.json())
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //routes
 const apiRoutes = require("./routes/apiRoutes")(app);
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
-})
+});
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
-})
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
